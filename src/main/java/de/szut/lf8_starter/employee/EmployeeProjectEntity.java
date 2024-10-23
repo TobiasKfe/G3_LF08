33e07174f0f07a35b1b749838377194c691a3ac8
Tobias Küfe <t.kuefe@neusta.de>
2024-10-23 09:19:54 +0200

    GLP-7 employeeService added

---
+++ src/main/java/de/szut/lf8_starter/employee/EmployeeProjectEntity.java
@@ -0,0 +1,21 @@
+package de.szut.lf8_starter.employee;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "employeeProject")
+public class EmployeeProjectEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private long employeeId;
+    private long projectId;
+}
